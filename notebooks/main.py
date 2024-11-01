fccbb62e92532e10ea477c33a14198cf3143e08d
ozilot <zoe.flumini@gmail.com>
2024-11-01 10:10:20 +0100

    random forest

--- notebooks/main.py
+++ notebooks/main.py
@@ -2,6 +2,11 @@
 Fragen:
 - wäre es wohl besser gewesen anstatt utples ein dictionary zu verwenden?!
 - eine website machen wo man ein model wählen kann, ein dataset und mit dingen wie anzahl nodes herumspielen
+- ein gntm machen statt mit menschenmodels languagemodels etc (bodytype=modeltype)
+
+Meine verschiedenen Models:
+- decisiontreeregressor (mit und ohne max_depth_value)
+- randomforestregressor (mit oder ohne max_depth_value?!)
 '''
 
 # import 
@@ -9,6 +14,7 @@ import pandas as pd # wenn man etwas as importaet dann kann man einen namen defi
 from sklearn.model_selection import train_test_split # funktion um die data zu splitten in trainings und validation data
 from sklearn.tree import DecisionTreeRegressor # der modeltyp wird von da geholt
 from sklearn.metrics import mean_absolute_error as mae # hiermit wird der mae berechnet
+from sklearn.ensemble import RandomForestRegressor 
 
 
 #======= Load and read Data  ========
@@ -39,16 +45,16 @@ X_train, X_test, y_train, y_test = train_test_split(features, target, random_sta
 
 #=======  Modell kreieren, fitten und predictions machen (allg.)  ========
 # choose a model
-model = DecisionTreeRegressor(random_state=0) # random_state=number: randomness wird verwendet bei der konstellation der verschiedenen datenpunkten || max_depth= ...als input kann man noch max_depth hineintun sobald man weiss wie viele knoten maximal der baum haben soll um under/overfitting zu vermeiden
+model1 = DecisionTreeRegressor(random_state=0) # random_state=number: randomness wird verwendet bei der konstellation der verschiedenen datenpunkten || max_depth= ...als input kann man noch max_depth hineintun sobald man weiss wie viele knoten maximal der baum haben soll um under/overfitting zu vermeiden
 # fit the model
-model.fit(X_train, y_train)
+model1.fit(X_train, y_train)
 # make predictions
-prediction = model.predict(X_test)
+prediction1 = model1.predict(X_test)
 # print(prediction)
 
 
 #=======  Abweichung berechnen allg. (MAE)  ========
-error = mae(prediction, y_test) # diese funktion berechnet den durchscnitt von allen differenzen zwischen prediction und dem eigentlichen targetwert der testdaten
+error1 = mae(prediction1, y_test) # diese funktion berechnet den durchscnitt von allen differenzen zwischen prediction und dem eigentlichen targetwert der testdaten
 # print(error)
 
 
@@ -84,12 +90,12 @@ def MAE(liste_mit_baumtiefen, X_train, y_train, X_test, y_test):
 
 
 
-# 1. Optimierung: Optimale Baumtiefe (anz. Nodes) finden
-
-
-
-
-
-    
+# 2. Optimierung: Random forests (besserer modeltyp; um die gesamtprediction des tarets zu berechnen wird das mittel von mehreren verschiedenen trees verwendet)
+# model definieren, fitten, predicten
+model2 = RandomForestRegressor(random_state=12) # braucht es hier auch max_depth_values?
+model2.fit(X_train, y_train)
+prediction2 = model2.predict(X_test)
+error2 = mae(prediction2, y_test)
+print(prediction2, error2)
 
 
